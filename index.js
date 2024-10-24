4498826b64fe18c0eb60571041cc1504d90a132d
Jesus Alejandro Artiaga Morales <jesusartiaga.chucho@gmail.com>
2024-10-24 16:42:57 -0600

    Practica 11
    Configuracion de Tailwwindcss

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const app = express()
 app.set('view engine', 'pug')
 app.set('Views', '/.Views')
 
+// ? Carpeta publica de recursos estaticos (assets)
+app.use(express.static('public'))
+
 const port = 3000
 
 app.listen(port, () =>
