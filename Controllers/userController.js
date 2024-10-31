0125ef1ff9b2332d642e7beff2d7744594f4c1b9
Jesus Alejandro Artiaga Morales <jesusartiaga.chucho@gmail.com>
2024-10-31 16:14:05 -0600

    Practica 11
    Creacion de formulario register con TailwindCSS y PUG

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,18 +1,18 @@
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
-        
+        page : 'Inicia Sesion'
     })
 }
 
 const formularioRegister = (req, res) => {
     res.render('auth/register', {
-        
+        page : 'Crear Cuenta'
     })
 }
 
 const formularioPasswordRecovery = (req, res) => {
     res.render('auth/passwordRecovery', {
-        
+        page : 'Recupera tu contraseña'
     })
 }
 
